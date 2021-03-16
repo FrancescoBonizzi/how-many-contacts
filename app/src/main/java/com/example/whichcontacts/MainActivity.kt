0490625367f1d7edd8a8766407057bf486987c79
Francesco Bonizzi <fbonizzi.90@gmail.com>
2021-03-16 07:36:20 +0100

    First commit

---
+++ app/src/main/java/com/example/whichcontacts/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.whichcontacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
